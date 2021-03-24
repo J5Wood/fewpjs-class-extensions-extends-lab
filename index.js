c4cef41f50e9e740af78a29bf2e190ddfb6cb30b
Jeremy Wood <js.wood206@gmail.com>
2021-03-23 20:53:49 -0700

    commit tree 1f661dc6eefccd0e87f45e603f158f41c81fde2c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        let perimLength = 0
+        for (const key in this.sides){
+            perimLength += this.sides[key]
+        }
+        return perimLength
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        if (this.sides[0] + this.sides[1] <= this.sides[2] || this.sides[1] + this.sides[2] <= this.sides[0] || this.sides[0] + this.sides[2] <= this.sides[1]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.sides.every(side => side === this.sides[0])
+    }
+
+    get area() {
+        return this.sides[0] * this.sides[1]
+    }
+}
